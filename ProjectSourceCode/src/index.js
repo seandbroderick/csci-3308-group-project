38ba253930b5b7e4e14c9fdff4706ae2711987b7
Ethan Meli <123030367+EthanMeli@users.noreply.github.com>
2025-04-16 15:00:44 -0600

    Update index.js

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -19,8 +19,8 @@ app.use('/resources', express.static(path.join(__dirname, 'resources')));
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
-  port: 5432, // the database port
+  host: process.env.POSTGRES_HOST, // the database server
+  port: process.env.PORT, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
   password: process.env.POSTGRES_PASSWORD, // the password of the user account
